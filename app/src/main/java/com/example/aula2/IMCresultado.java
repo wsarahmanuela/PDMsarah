df77a88efcadfcdbaf21910fd2efd5ba2e529380
sarah <sarahmanuela313@gmail.com>
2025-09-08 09:11:15 -0300

    calculadora

--- app/src/main/java/com/example/aula2/IMCresultado.java
+++ app/src/main/java/com/example/aula2/IMCresultado.java
@@ -1,7 +1,6 @@
 package com.example.aula2;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button button;
+public class IMCresultado extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        button.setOnClinckListener(View v ->{
-
-        })
+        setContentView(R.layout.activity_imcresultado);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
