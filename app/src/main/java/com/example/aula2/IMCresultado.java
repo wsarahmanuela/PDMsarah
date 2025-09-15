4d3cb1db2f47dd686e602ab5e68d3dbb9be5f9ae
sarah <sarahmanuela313@gmail.com>
2025-09-15 09:13:05 -0300

    peso

--- app/src/main/java/com/example/aula2/IMCresultado.java
+++ app/src/main/java/com/example/aula2/IMCresultado.java
@@ -1,6 +1,9 @@
 package com.example.aula2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class IMCresultado extends AppCompatActivity {
 
+    Button b;
+
+    TextView imageView;
+
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_imcresultado);
+        tvResultado=findViewById(R.id.textView);
+        Intent i =getIntent();
+        Bundle b =i.getExtras();
+        Double peso = b.getDouble("peso");
+        Double altura = b.getDouble("altura");
+
+        double ima  = peso/(altura*altura);
+
+        tvResultado.setText(Double.toHexString(imc));
+
+        imageView.set
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.aula2;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,26 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    EditText edPeso, edAltura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        button.setOnClinckListener(View v ->{
-
-        })
+        button=findViewById(R.id.button4);
+        edPeso=findViewById(R.id.edPeso);
+        edAltura=findViewById(R.id.edAltura);
+        button.setOnClickListener( v  -> {
+            Intent i = new Intent(MainActivity.this, MainActivity.class);
+            Bundle bundle = new Bundle();
+            Double peso = Double.parseDouble(edPeso.getText().toString());
+            Double altura = Double.parseDouble(edAltura.getText().toString());
+            bundle.putDouble("peso",peso);
+            bundle.putDouble("altura", altura);
+            i.putExtras(bundle);
+            startActivity(i);
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
