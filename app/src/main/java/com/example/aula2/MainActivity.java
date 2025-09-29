c974ef09c396446e6fb3cadf0576ae366fbababd
sarah <sarahmanuela313@gmail.com>
2025-09-29 09:02:16 -0300

    array

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aula2;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    String[] nomes = new String[]{"Cesar", "Jose","Romulo", "Clara","Samanta","Gustavo"};
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listview);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ArrayAdapter<String>adapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,android.R.id.text1,nomes);
+        listView.setAdapter(adapter);
     }
 }
