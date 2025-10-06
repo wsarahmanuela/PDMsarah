3c6228486bcc2feb4b0cf6e48c2de95bc76d4900
sarah <sarahmanuela313@gmail.com>
2025-10-06 09:05:12 -0300

    listagem

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.aula2;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,21 +17,31 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] nomes = new String[]{"Cesar", "Jose","Romulo", "Clara","Samanta","Gustavo"};
+    ArrayList<String> nomes;
     ListView listView;
+    Button button;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         listView=findViewById(R.id.listview);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        ArrayAdapter<String>adapter = new ArrayAdapter<>(getApplicationContext(),
-                android.R.layout.simple_list_item_1,android.R.id.text1,nomes);
+        button=findViewById(R.id.button);
+        editText=findViewById(R.id.editText);
+        nomes = new ArrayList<String>(); //Inicializa ArrayList
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         listView.setAdapter(adapter);
+        button.setOnClickListener(v-> {
+            nomes.add(editText.getText().toString());
+            adapter.notifyDataSetChanged();
+        });
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            nomes.remove(position);
+            adapter.notifyDataSetChanged();
+            return true;
+        });
+
     }
 }
