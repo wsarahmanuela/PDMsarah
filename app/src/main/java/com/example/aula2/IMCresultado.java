39722fa9b67abe33cfacf9e55a2e44d8ea6c3432
Your Name <sarahmanuela313@gmail.com>
2025-12-01 08:00:20 -0300

    hnnnnnnnnnnnnn

--- app/src/main/java/com/example/aula2/IMCresultado.java
+++ app/src/main/java/com/example/aula2/IMCresultado.java
@@ -2,6 +2,7 @@ package com.example.aula2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,10 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class IMCresultado extends AppCompatActivity {
 
-    Button b;
-
-    TextView imageView;
-
+    Button button;
+    TextView tvResultado;
     ImageView imageView;
 
     @Override
@@ -24,22 +23,38 @@ public class IMCresultado extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_imcresultado);
-        tvResultado=findViewById(R.id.textView);
-        Intent i =getIntent();
-        Bundle b =i.getExtras();
-        Double peso = b.getDouble("peso");
-        Double altura = b.getDouble("altura");
+        tvResultado=findViewById(R.id.tvResultado);
+        imageView=findViewById(R.id.imageView2);
 
-        double ima  = peso/(altura*altura);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        Double peso = bundle.getDouble("peso");
+        Double altura = bundle.getDouble("altura");
 
-        tvResultado.setText(Double.toHexString(imc));
+        Double imc = peso/(altura*altura);
 
-        imageView.set
+        tvResultado.setText(Double.toString(imc));
+        imageView.setImageResource(R.drawable.perfil);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        if(imc < 18.5){
+            imageView.setImageResource(R.drawable.abaixopeso);
+        }
+        if(imc >= 18.5 && imc < 25){
+            imageView.setImageResource(R.drawable.normal);
+        }
+        if(imc >= 25 && imc < 30){
+            imageView.setImageResource(R.drawable.sobrepeso);
+        }
+        if(imc >= 30 && imc < 35){
+            imageView.setImageResource(R.drawable.obesidade1);
+        }
+        if(imc >= 35 && imc < 40){
+            imageView.setImageResource(R.drawable.obesidade2);
+        }
+        if(imc >= 40){
+            imageView.setImageResource(R.drawable.obesidade3);
+        }
+
+
     }
 }
