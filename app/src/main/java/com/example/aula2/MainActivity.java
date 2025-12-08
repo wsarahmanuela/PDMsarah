c06609529f3c057ce381f94e613940e323c6d819
sarah <sarahmanuela313@gmail.com>
2025-12-08 08:01:03 -0300

    jjjj

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -21,25 +21,20 @@ public class MainActivity extends AppCompatActivity {
             "Maria Clara", "Clara"
     };
 
-    ListView lv; // ListView onde os nomes vão aparecer
+    ListView lv; //lista
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Carrega o layout da tela principal
+        setContentView(R.layout.activity_main); //layout da tela principal
+        lv = findViewById(R.id.listview);//recupera
 
-        // Recupera a ListView do layout XML e liga a variável lv
-        lv = findViewById(R.id.listview);
-
-        // Criamos o Adapter que vai conectar os nomes ao layout de cada item da lista
         ArrayAdapter<String> a = new ArrayAdapter(
                 this,
-                R.layout.item_lista, // Layout do item individual da lista
-                R.id.textView, // TextView dentro do layout que vai receber cada nome
-                nomes // Vetor de nomes que será usado como dados
+                R.layout.item_lista,
+                R.id.textView,//ta recebendo aqui
+                nomes
         );
-
-        // Define o adapter para exibir os nomes na ListView
         lv.setAdapter(a);
     }
 }
